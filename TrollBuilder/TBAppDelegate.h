fa772fcde766392e52165cfbcc26a01cd9da8b39
Karl Syvert Løland <karl@altinett.no>
2012-04-21 01:19:48 +0200

    Project init

---
+++ TrollBuilder/TBAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  TBAppDelegate.h
+//  TrollBuilder
+//
+//  Created by Karl Løland on 4/21/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TBAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+- (IBAction)saveAction:(id)sender;
+
+@end
