6da0e2e837c2d15c917b10e940d09566adb972d3
Karl Syvert Løland <karl@altinett.no>
2012-08-19 17:12:29 +0200

    new custom objective-git

---
+++ TrollBuilder/RemoteDialogWindowController.h
@@ -0,0 +1,13 @@
+//
+//  RemoteDialogWindowController.h
+//  TrollBuilder
+//
+//  Created by Karl Løland on 8/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RemoteDialogWindowController : NSWindowController
+
+@end
---
+++ TrollBuilder/RemoteDialogWindowController.m
@@ -0,0 +1,34 @@
+//
+//  RemoteDialogWindowController.m
+//  TrollBuilder
+//
+//  Created by Karl Løland on 8/19/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "RemoteDialogWindowController.h"
+
+@interface RemoteDialogWindowController ()
+
+@end
+
+@implementation RemoteDialogWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
